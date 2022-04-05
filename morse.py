eebb835573b2850bd1c9eb72ec367882b0cc8a77
Adam Tannir <atannir@gmail.com>
2022-04-05 11:14:59 -0400

    Encode working, decode for 1 character working

--- morse.py
+++ morse.py
@@ -3,11 +3,25 @@
 # 
 
 import argparse
+import sys
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-e","--encode", default=True, required=False, help="Output Morse code for input string. This is the default.")
-ap.add_argument("-d", "--decode", default=False, required=False, help="Output text from Morse code input.")
+mode_arg = ap.add_mutually_exclusive_group()
+mode_arg.add_argument("-e","--encode", default=True, action="store_true", required=False, help="Output Morse code for input string. This is the default.")
+mode_arg.add_argument("-d", "--decode", default=False, action="store_true", required=False, help="Output text from Morse code input.")
+#ap.add_argument("-e","--encode", default=True, required=False, help="Output Morse code for input string. This is the default.")
+#ap.add_argument("-d", "--decode", default=False, required=False, help="Output text from Morse code input.")
+ap.add_argument("cmdargs", nargs=argparse.REMAINDER)
 args = vars(ap.parse_args())
+
+#print(args)
+
+if (args["encode"] == True):
+    mode = "encode"
+if (args["decode"] == True):
+    mode = "decode" 
+
+#print(mode)
 
 morse_dict = {
     "A": ".-",
@@ -36,11 +50,56 @@ morse_dict = {
     "X": "-..-",
     "Y": "-.--",
     "Z": "--..",
+    " ": "   "
 }
 
-test_string = "HELLO WORLD"
+# reverse keys and values, easier than looking through each time
+reverse_m_dict = {v: k for k, v in morse_dict.items()}
 
-def decode_letter(l):
+test_string = "HELLO WORLD!"
+test_morse = ".... . .-.. .-.. ---     .-- --- .-. .-.. -.."
+
+def encode_letter(l):
+    l = l.upper()
     # return the value from the key
-    #if()
-    return ""
+    if(l in morse_dict):
+        return morse_dict[l]
+    return l
+
+def decode_morse(m):
+    # spaces already stripped out
+    #if(m in morse_dict.values()):
+    if(m in reverse_m_dict):
+        return reverse_m_dict[m]
+    return m
+
+#print(encode_letter("e"))
+#print(decode_morse(test_morse))
+
+# if(args["cmdargs"]):
+#     input_string = args["cmdargs"][0]
+# else:
+#     input_string = test_string
+output_string = ''
+#print(input_string)
+if(mode == 'encode'):
+    if(args["cmdargs"]):
+        input_string = args["cmdargs"][0]
+    else:
+        input_string = test_string
+    for c in input_string:
+        #print(encode_letter(c) + " ", end="") # leaves trailing space
+        output_string += (encode_letter(c) + " ")
+    print(output_string)
+    sys.exit()
+
+if(mode == 'decode'):
+    if(args["cmdargs"]):
+        input_string = args["cmdargs"][0] # will only decode one character
+    else:
+        input_string = test_morse
+    for m in input_string.split(" "):
+        #print(decode_morse(c), end="") # space between words gets consumed
+        output_string += (decode_morse(m))
+    print(output_string)
+    sys.exit()
